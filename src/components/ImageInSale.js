d19a87169eed43880e7bb352a1bbcc0211caf6ff
stormyRay <raymond90523@gmail.com>
2017-02-08 23:33:06 +0800

    Add Theme and ImageInSale contents

--- src/components/ImageInSale.js
+++ src/components/ImageInSale.js
@@ -1,1 +1,17 @@
-ImageInSale.js
+import React from "react";
+
+class ImageInSale extends React.Component{
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="image-in-sale-container">
+				AN IMAGE
+			</div>
+		)
+	}
+}
+
+export default ImageInSale;
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,1 +1,17 @@
-Theme.js
+import React from "react";
+
+class Theme extends React.Component{
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="theme-container">
+				A THEME
+			</div>
+		)
+	}
+}
+
+export default Theme;
