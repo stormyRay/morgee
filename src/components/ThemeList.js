1e063bac17886f5629c5190777270edd13b99aec
stormyRay <raymond90523@gmail.com>
2017-02-11 23:08:48 +0800

    remove merge marks

--- src/components/ThemeList.js
+++ src/components/ThemeList.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import {SEARCH_BY_THEME} from "../constants/texts";
-=======
-import {SEARCH_BY_THEME} from "../contsants/text";
->>>>>>> 763900616e762553d4952b93dda522fe9d4647e7
 
 class ThemeList extends React.Component{
 	constructor(props) {
@@ -12,13 +8,8 @@ class ThemeList extends React.Component{
 
 	render() {
 		return (
-<<<<<<< HEAD
 			<div className="container theme-list-container">
 				<div className="header theme-list-header">
-=======
-			<div className="theme-list-container">
-				<div className="theme-list-header">
->>>>>>> 763900616e762553d4952b93dda522fe9d4647e7
 					<h3>{SEARCH_BY_THEME}</h3>
 				</div>
 				<div className="theme-list-content"></div>
