6a7e1571a1be3c08d80cf7dc6f3e8f125cfaeb91
stormyRay <raymond90523@gmail.com>
2017-02-22 23:33:13 +0800

    Update cloth type setting panel component

--- src/components/setting-panel/ClothTypes.js
+++ src/components/setting-panel/ClothTypes.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Nav from "react-bootstrap/lib/Nav";
+import NavItem from "react-bootstrap/lib/NavItem";
+
+import {MALE_NORMAL, MALE_FIT, FEMALE_LOOSE, FEMALE_NORMAL} from "../constants/texts";
 
 class ClothTypes extends React.Component{
 	constructor(props) {
@@ -6,9 +10,35 @@ class ClothTypes extends React.Component{
 	}
 
 	render(){
+		const {clothType, swicthClothType} = this.props;
 		return (
 			<div className="setting-panel-container">
-				ClothTypes
+				<Nav bsStyle="pills" justified activeKey={clothType} onSelect={swicthClothType}>
+					<NavItem eventKey="male_normal">
+						<div className="customize-tab-wrapper">
+							<span className="sprite-image male-normal" />
+							{MALE_NORMAL}
+						</div>
+					</NavItem>
+					<NavItem eventKey="male_fit">
+						<div className="customize-tab-wrapper">
+							<span className="sprite-image male-fit" />
+							{MALE_FIT}
+						</div>
+					</NavItem>
+					<NavItem eventKey="female_loose">
+						<div className="customize-tab-wrapper">
+							<span className="sprite-image male-loose" />
+							{FEMALE_LOOSE}
+						</div>
+					</NavItem>
+					<NavItem eventKey="female_normal">
+						<div className="customize-tab-wrapper">
+							<span className="sprite-image female-normal" />
+							{FEMALE_NORMAL}
+						</div>
+					</NavItem>
+				</Nav>
 			</div>
 			)
 	}
