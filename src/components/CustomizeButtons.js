46e734fdfce45cd2d1e1997374b2099be49f0608
stormyRay <raymond90523@gmail.com>
2017-02-26 19:19:35 +0800

    Fix bug on resizing of the screen

--- src/components/CustomizeButtons.js
+++ src/components/CustomizeButtons.js
@@ -4,15 +4,22 @@ import {SELECT_OTHER_IMAGE, COMPLETE_CUSTOMIZE} from "../constants/texts";
 class CustomizeButtons extends React.Component{
 	constructor(props) {
 		super(props);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	render(){
 		return (
 			<div className="customize-buttons-container">
-				<button className="customize-button customize-button-back" onClick={()=>{window.history.back();}}>{SELECT_OTHER_IMAGE}</button>
+				<button className="customize-button customize-button-back" onClick={this.handleClick}>{SELECT_OTHER_IMAGE}</button>
 				<button className="customize-button customize-button-complete">{COMPLETE_CUSTOMIZE}</button>
 			</div>
 			)
+	}
+
+	handleClick(){
+		if(!window.history.back()){
+			location.href = "/index";
+		}
 	}
 }
 
--- src/components/TshirtSampleArea.js
+++ src/components/TshirtSampleArea.js
@@ -5,30 +5,40 @@ class TshirtSampleArea extends React.Component{
 	constructor(props) {
 		super(props);
 
+		this.shirtUrl = "";
+		this.state = {
+			wrapperStyle: {}
+		};
+
 		this.buildImageSrc = this.buildImageSrc.bind(this);
 		this.buildTshirtSrc = this.buildTshirtSrc.bind(this);
 		this.computeSize = this.computeSize.bind(this);
 		this.computePositionY = this.computePositionY.bind(this);
+		this.computeWrapperSize = this.computeWrapperSize.bind(this);
 	}
 
 	render(){
 		const {clothType, clothColor, imageSize, imagePosition} = this.props.settings;
 		const {imageId} = this.props;
 		const shirtStyle = {
-			backgroundImage: "url(" + this.buildTshirtSrc(clothType, clothColor)+")"
+			backgroundImage: "url(" + this.buildTshirtSrc(clothType, clothColor) +")"
 		};
 		const imageStyle = {
 			backgroundSize: this.computeSize(imageSize) + " auto", 
 			backgroundImage: "url(" + this.buildImageSrc(imageId) +")", 
 			backgroundPositionY: this.computePositionY(imagePosition)
 		};
+		this.computeWrapperSize();
+		const wrapperStyle = this.state.wrapperStyle;
 		
 		return (
 			<div className="shirt-showing-area">
 				<div className="shirt-container">
 					<div className="shirt-background" style={shirtStyle} >
-						<div className="image-wrapper">
-							<div className="printed-image" style={imageStyle}>
+						<div className="shirt-area"style={wrapperStyle}>
+							<div className="image-wrapper">
+								<div className="printed-image" style={imageStyle}>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -42,6 +52,7 @@ class TshirtSampleArea extends React.Component{
 		var src = "/images/T-shirts/";
 		src += type.replace("_", "-");
 		src += "/" + color + ".png";
+		this.shirtUrl = src;
 
 		return src;
 	}
@@ -56,6 +67,37 @@ class TshirtSampleArea extends React.Component{
 
 	buildImageSrc(id){
 		return "/images/" + id + ".jpg";
+	}
+
+	computeWrapperSize(){
+		var rawImage = new Image();
+		rawImage.src = this.shirtUrl;
+
+		rawImage.onload = function(){
+			var imageWidth = rawImage.width;
+			var imageHeight = rawImage.height;
+			var imageRatio = imageWidth / imageHeight;
+
+			var containerWidth = $(".shirt-background").width();
+			var containerHeight = $(".shirt-background").height();
+			var containerRatio = containerWidth / containerHeight;
+
+			if(imageRatio >= containerRatio){
+				this.setState({
+					wrapperStyle:{
+						width: "100%",
+						height: 100 / imageRatio * containerRatio+ "%"
+					}
+				});
+			} else {
+				this.setState({
+					wrapperStyle:{
+						width: 100 * imageRatio /containerRatio + "%",
+						height: "100%"
+					}
+				});
+			}
+		}.bind(this);
 	}
 }
 
