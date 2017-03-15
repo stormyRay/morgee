4f5652020e066c5de463003eb65b526dbe4537ae
stormyRay <raymond90523@gmail.com>
2017-03-15 23:57:48 +0800

    Add tshirt number

---
+++ src/components/customer-info/TshirtNumber.js
@@ -0,0 +1,16 @@
+import React from "react";
+class TshirtNumber extends React.Component{
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+		return (
+			<div>
+			TshirtNumber
+			</div>
+		)
+	}
+}
+
+export default TshirtNumber;
